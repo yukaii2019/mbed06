73d6cd58da8e9150ac3dbfdd8121cddf5dc15363
Yukai Hsieh <yukai09151702@gmail.com>
2020-04-07 23:37:07 +0800

    add main.cpp

---
+++ 6_3_UART_Decoding/main.cpp
@@ -0,0 +1,14 @@
+#include "mbed.h"
+Serial device(D12,D11);
+int main(){
+    char cmd[10] = "PicoScope";
+    int i = 0;
+    while(1){
+        device.baud(9600);
+        device.putc(cmd[i]);
+        i++;
+        if(i==10){
+            i=0;
+        }
+    }
+}
